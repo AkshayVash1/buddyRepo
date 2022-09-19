f2ab3f96b6c75e8e19b097d6193f62ebbb907194
Akshay <akshayvashisht15@gmail.com>
2022-09-19 17:02:30 -0400

    new buddy added.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,6 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddressBook {
 
+    private List<BuddyInfo> list;
+
+    public AddressBook(){
+        List<BuddyInfo> list = new ArrayList<>();
+    }
+
     public static void main (String[] args){
-        System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Tom", "613", "Carleton");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
+    }
+
+    public void addBuddy(BuddyInfo buddy){
+        list.add(buddy);
+    }
+
+    public void removeBuddy(BuddyInfo buddy){
+        list.remove(buddy);
     }
 }
---
+++ src/BuddyInfo.java
@@ -0,0 +1,43 @@
+public class BuddyInfo {
+
+    private String name;
+    private String phoneNumber;
+    private String address;
+
+    public BuddyInfo(String name, String phoneNumber, String address) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public static void main(String[] args) {
+
+        BuddyInfo A = new BuddyInfo("Akshay", "123-456-7891", "123 Way");
+
+        System.out.println("Hello " + A.getName());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
