35e855bbb7b89f0754187509776bd4c972a56312
akshay <akshayvashisht15@gmail.com>
2022-09-26 14:54:56 -0400

    new buddy added.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,26 +1,33 @@
 import java.util.ArrayList;
-import java.util.List;
+
 
 public class AddressBook {
 
-    private List<BuddyInfo> list;
+    private ArrayList<BuddyInfo> myBuddies;
 
     public AddressBook(){
-        List<BuddyInfo> list = new ArrayList<>();
+        myBuddies = new ArrayList<>();
+    }
+
+    public void addBuddy(BuddyInfo buddy){
+        if (buddy != null) {
+            myBuddies.add(buddy);
+        }
+    }
+
+    public BuddyInfo removeBuddy(int index){
+        if (index >= 0 && index < myBuddies.size()) {
+            return myBuddies.remove(index);
+        }
+        return null;
     }
 
     public static void main (String[] args){
         BuddyInfo buddy = new BuddyInfo("Tom", "613", "Carleton");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
-        addressBook.removeBuddy(buddy);
-    }
-
-    public void addBuddy(BuddyInfo buddy){
-        list.add(buddy);
-    }
-
-    public void removeBuddy(BuddyInfo buddy){
-        list.remove(buddy);
+        addressBook.removeBuddy(1);
+        BuddyInfo buddy2 = new BuddyInfo("Jim", "271", "Carleton");
+        addressBook.addBuddy(buddy2);
     }
 }
